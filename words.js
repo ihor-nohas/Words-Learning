a3ed03caecfb3ad09288ec5102c7f71297c97eb9
ihor-nohas <ihor.nogas@gmail.com>
2025-03-27 21:04:05 +0100

    Initial commit

---
+++ words.js
@@ -0,0 +1,181 @@
+let inputString = 
+/*because, as, since	тому що, оскільки		
+simply divine	просто божественно		
+I'm pretty good	Я дуже добре		
+Not to bad	He погано		
+Can't complain	He скаржусь		
+Uh, don't ask	не питай		
+same old	нічого нового, все по старому		
+pleasant	приємний		
+unpleasant	неприємний		
+timetables	розклади 
+schedules	графіки		
+set off	графік		
+yearly	Щорічно		
+tend to	бути схильним до		
+day off	відгул (вихідний в робочий день)		
+duration	тривалість		
+since	з якогось моменту		
+for	напротязі		
+ever	коли небуть		
+youth	молоді		
+cover	втягнутий, занурений, захоплений		
+engage	втягнутий, занурений, захоплений		
+make somebody	примушувати когось		
+force somebody	примушувати когось		
+overdrank	перепив		
+thirsty	спраглий		
+species	видів		
+outskirts	околиці, окраїна		
+beetroot	буряк		
+team up	об'єднатися		
+it's not my cup of tea	це не мое		
+bodyshop	бордель		
+sexual intercourse	статевий акт		
+accurate	точний		
+express	виражати		
+fluently	вільно		
+mastering	вдосконалювати		
+picked up	підбирати, підвозити		
+struggled	докладати зусиль		
+get by	бути зрозумілим		
+rumour	чутки		
+gossip	плітки		
+oven	духовка, піч		
+training session	тренувальна сесія		
+researching	дослідження		
+researcher	дослідник		
+attend	відвідувати		
+convenient	зручний		
+pistachio	фісташка		
+find out	взнавати, з'ясувати	*/	
+`follow up	слідувати		
+involve	втягувати, залучати		
+as if	неначе, ніби		
+taken up	зайнятий, взято		
+it seems to me as if	мені здасться ніби		
+to be worthy	краще робити ніж ні, бути гідним		
+bigoted	фанатичний		
+stress	наголос, стрес		
+loathe	ненавидіти, огида		
+iron	залізо, метал, праска		`;
+/*investigates	досліджувати, розслідує		
+priest	священик		
+among	серед		
+though	хоча		
+hold	тримати, утримуйте		
+suggest	припустити, пропунувати		
+far	далеко		
+certainly	звичайно		
+exaggerated	перебільшений, перебільшено		
+therefore	тому, отже		
+struggling	бореться		
+appreciating	оцінюючи, цінуючи		
+merely	просто		
+according	відповідно до, згідно		
+sufficient	достатній, достатньо		
+evidence	докази		
+argues	міркує, стверджує, сперечається		
+achievements	досягнення		
+conclusions	висновки		
+advantages	переваги		
+genes	гени		
+allow	дозволити		
+posses	володіти, володіє		
+efficient	ефективний		
+performer	виконавець		
+instead	натомість		
+opportunities	можливості		
+techniques	методи, техніки		
+appear	з'являються		
+in terms of	з точки зору		
+marely	майже не, марно		
+extraordinary	незвичний, надзвичайний		
+robe	халат, мантія		
+draw	тягнути, нічия, малювати		
+pull	тягнути		
+sympathetic	співчутливий		
+patient	терплячий, пацієнт		
+realised	усвідомив, зрозумів		
+goods	товари, товарів		
+indeed	насправді, справді		
+mean	злий, жадібний, маю на увазі		
+suppose	думаю, припустимо		
+float	парить, плавати		
+persuade	переконати		
+cheeky	неслухняний, зухвалий		
+naughty	неслухняний		
+mischievous	неслухняний, пустотливий		
+disobedient	неслухняний		
+badly behaved	неслухняний, погано поводився		
+gambling	азартні ігри		
+trunk	хобот, стовбур, багажник		
+log	поліно, журнал		
+exhausted	виснажений, дуже втомлений		
+delightit	піднесений, радість		
+confused	спантеличиний, розгублений		
+fed up	втомлений, набридло		
+frown	мружитися, хмуриться		
+annoyed	роздратований		
+pleased	задоволений		
+furious	розлючений, лютий		
+Upset	засмучений		
+guilty	винний		
+in a good mood	в хорошому настрої		
+pleasantly surprised	приємно здивований		
+jolly holiday	веселі вихідні, веселе свято		
+liquor store	магазин алкогольних напоїв		
+apparently	очевидно, мабуть		
+accommodation	житло, помешкання, розміщення		
+relief	полегшення		
+look as if	виглядаєш як, виглядати ніби		
+how come	як же так		
+go on	давай, продовжувати		
+cheer you up	підбадьорити		
+what a pity	як шкода		
+what a pain	як шкода, який біль		
+common	поширений, загальний		
+hug	обійняти, обійми		
+habbit	звичка		
+meaningful connections	значущі зв'язки		
+desperate attempt	відчайдушна спроба		
+drastic	радикальний, кардинальний, різкий		
+anxious about	схвильований, стурбований		
+viral	вірусний		
+consequences	наслідки		
+impact	наслідки, вплив		
+effect	наслідки, ефект		
+continuity	безперервність		
+plural	множини		
+duvet	ковдра		
+loaf	буханець. коровай		
+loaves	булки, батони		
+My friend and I	Ми з другом		
+Me and my friend	Я і мій друг		
+overeat	переїдати		
+weather forecast	прогноз погоди		
+implement	впровадити		
+foreign	іноземені		
+accent	акцент		
+hyperglot	гіперглот		
+globalization	глобалізація		
+graveyard	кладовище		
+cemetery	цвинтар		
+get rich	стати багатим		
+civil servant	держслужбовець		
+warehouse	склад		
+separate	окремо		
+servant	слуга		
+auxiliary verbs	допоміжні дієслова		
+sweet tooth	ласун, сладкоіжка		
+follow-up questions	додаткові запитання		
+it takes	це займає		
+dial your number	набери свій номер		
+require	вимагають		
+ferry	пором		
+leaflet	листівка		
+nausea	нудота		
+devastated	спустошений		
+via	через		
+dungeon	підземелля		
+maintain	підтримувати */
